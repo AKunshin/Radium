3919cd3607277a05cdeae6ee2e2a9542a646ce78
AKunshin <reyiby2015@gmail.com>
2023-03-27 13:43:58 +0300

    Download and unzip files from url-link

---
+++ main.py
@@ -0,0 +1,20 @@
+from io import BytesIO
+import requests, zipfile
+
+
+def download_a_files(url: str):
+    response = requests.get(url)
+
+    # extracting the zip file contents
+    zf= zipfile.ZipFile(BytesIO(response.content))
+    zf.extractall('./downloads')
+
+
+def main():
+    url = 'https://gitea.radium.group/radium/project-configuration/archive/master.zip'
+    download_a_files(url)
+
+if __name__ == '__main__':
+    main()
+
+
